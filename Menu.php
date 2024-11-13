9487192292a873b53881c4361511715838fbda9e
MARCEL HERNANDES SANT ANNA <marcel.hsanna@senacsp.edu.br>
2024-11-12 21:27:31 -0300

    Terceiro Commit

---
+++ Menu.php
@@ -0,0 +1,15 @@
+<?php
+    namespace PHP\Modelo;
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Menu</title>
+</head>
+<body>
+    <a href="Inserir.php"><button>Cadastrar</button></a>
+    <a href="ConsultarCodigo.php"><button>Consultar</button></a>
+    <a href="PaginaAtualizar.php"><button>Atualizar</button></a>
+    <a href="PaginaExcluir.php"><button>Excluir</button></a>
+</body>
+</html>
--- main.php
+++ main.php
@@ -7,6 +7,9 @@
     require_once('DAO/Conexao.php');
     require_once('DAO/Inserir.php');
     require_once('DAO/Consultar.php');
+    require_once('DAO/ConsultarCodigo.php');
+    require_once('DAO/Atualizar.php');
+    require_once('DAO/Excluir.php');
     use PHP\Modelo\Cliente;//Definir a classe
     use PHP\Modelo\Pessoa;
     use PHP\Modelo\Funcionario;
@@ -14,6 +17,9 @@
     use PHP\Modelo\DAO\Conexao;
     use PHP\Modelo\DAO\Inserir;
     use PHP\Modelo\DAO\Consultar;
+    use PHP\MOdelo\DAO\ConsultarCodigo;
+    use PHP\Modelo\DAO\Atualizar;
+    use PHP\Modelo\DAO\Excluir;
 
     /*
 
@@ -42,8 +48,22 @@
 
     //$inserir = new Inserir();//Criando acesso a classe inserir
     //echo $inserir->CadastrarPessoa($conn,"1","Fabio","Rua","11");
+    $conn = new Conexao();
+    $conn->conectar(); //Acessar o banco de dados
 
+   /* $inserir = new Inserir();
+    echo $inserir->cadastrarPessoa($conn,"1", "Fabio", "Rua","11");
+   */
     $consultar = new Consultar();
     echo $consultar->consultarIndividual($conn,'1');
+    echo $consultar->consultarIndividual($conn,'2');
+    echo $consultar->consultarIndividual($conn,'3');
 
+    $atualizar = new Atualizar();
+    echo $atualizar->atualizarPessoa($conn, "nome", "Allan", 1);
+
+    $excluir = new Excluir();
+    $excluir->excluirPessoa($conn,1);
+
+    
 ?>
